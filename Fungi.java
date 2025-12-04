d9306ce38370c02f83c80894dce723104f1cf645
HamDerY2 <yuni@itu.dk>
2025-12-04 14:55:30 +0100

    Update Fungi.java

--- Fungi.java
+++ Fungi.java
@@ -1,3 +1,4 @@
+import itumulator.executable.DisplayInformation;
 import itumulator.simulator.Actor;
 import itumulator.world.World;
 import itumulator.world.Location;
@@ -7,6 +8,8 @@ import java.util.*;
 public class Fungi implements Actor {
     int damage = 15;
     Carcass host;
+    int energy = 0;
+
     
 
     Fungi(Carcass host) {
@@ -14,11 +17,30 @@ public class Fungi implements Actor {
     }
     @Override
     public void act(World world) {
+        Random r = new Random();
+        energy += 5;
         if(world.contains(host)) {
             this.dealDamage(host);
         }
+
+        if(!world.contains(host)) {
+            Location fungiLoc = world.getLocation(this);
+            Set<Location> visibleTiles = world.getSurroundingTiles(fungiLoc, 3);
+            Set<Carcass> nearbyCarcasses = world.getAll(Carcass.class, visibleTiles);
+            List<Carcass> nearbyCarcassList = new ArrayList<>(nearbyCarcasses);
+            if(!nearbyCarcassList.isEmpty()) {
+                
+                Carcass target = nearbyCarcassList.get(r.nextInt(nearbyCarcassList.size()));
+                while(target.hasInternalFungi()) {
+                    nearbyCarcassList.remove(target);
+                    target = nearbyCarcassList.get(r.nextInt(nearbyCarcassList.size()));
+
+                }
+                target.infectWithFungi();
+            }
+        }
     }
     public void dealDamage(Carcass target) {
-        target.depleteMeat();
+        target.consume(world ,this.damage);
     }
 }
