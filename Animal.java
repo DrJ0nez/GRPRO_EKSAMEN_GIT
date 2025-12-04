a2235a750567b7691b983ce38e0e7f608f9f7680
HamDerY2 <yuni@itu.dk>
2025-12-04 13:21:14 +0100

    update animal

--- Animal.java
+++ Animal.java
@@ -3,7 +3,7 @@ import itumulator.world.Location;
 import itumulator.simulator.Actor;
 import java.util.Set;
 
-public abstract class Animal implements Actor {
+public abstract class Animal implements Actor { //test
     protected int hunger;
     protected int energy;
     protected int age = 0;
